1df728010b02ddc249ec0acde62470aeb9e97893
Manuel Mutzus <elmutzus@gmail.com>
2017-12-31 17:45:55 -0600

    Adding folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import components
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+// Needed for form data binding
+import { FormsModule } from '@angular/forms';
+
+// Needed for routing
 import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
+// Needed to use animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Needed to use services
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
